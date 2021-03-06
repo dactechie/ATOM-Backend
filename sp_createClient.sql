2d0822c5b3c4186476a6c1590d606099a9344c37
dactechie <directionshealth.forms@gmail.com>
2021-03-06 23:58:43 +1100

    Insert Client DOB,Sex  from SLK and Idempotent

--- sp_createClient.sql
+++ sp_createClient.sql
@@ -1,6 +1,27 @@
 CREATE PROCEDURE sp_createClient ( @SLK varchar(14), @ClientID int output)
 as 
 begin
-	insert into Client (SLK) values (@SLK)
-	set @ClientID = 1
+ 
+	select @ClientID = ID from Client where SLK = @SLK
+	IF @ClientID is null
+	 	BEGIN
+			DECLARE @IDs TABLE(ID INT);
+			DECLARE @datestring as varchar(10);
+			-- ABCDE123456781
+			set @datestring = SUBSTRING(@SLK, 6, 8);
+
+			insert into Client (SLK, DOB, Sex) 
+			OUTPUT inserted.ID INTO @IDs(ID)
+			values (
+				@SLK,
+				CONVERT(date,
+						SUBSTRING(@datestring, 5, 4) 
+						+ SUBSTRING(@datestring, 3, 2) 
+						+ SUBSTRING(@datestring, 1, 2)
+				),
+				CAST(SUBSTRING(@SLK, 14, 1) as TINYINT)
+			)
+			SELECT @ClientID = ID FROM @IDs;
+		END
+	
 end
